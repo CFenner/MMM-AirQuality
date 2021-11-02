007792452230ac922a0834c2fc445a8c283f47c1
cslev <cs.lev@gmx.com>
2021-11-02 19:59:03 +0800

    Add header features

--- MMM-AirQuality.js
+++ MMM-AirQuality.js
@@ -12,7 +12,9 @@ Module.register('MMM-AirQuality', {
 		showLocation: true,
 		showIndex: true,
 		updateInterval: 30, // every 30 minutes
-		animationSpeed: 1000
+		animationSpeed: 1000,
+		headerNeeded: false,
+		header: ''
 	},
 	start: function(){
 		Log.info('Starting module: ' + this.name);
@@ -55,6 +57,20 @@ Module.register('MMM-AirQuality', {
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
+		if(this.config.headerNeeded)
+		{
+			//do we want header?
+			var header = document.createElement("header");
+			var name = document.createElement("table");
+			name.classList.add("tiny", "table");
+			name.innerHTML = "<tr><td class='header_location'>" + this.config.header + "</td>" + 
+							 "<td>" + this.html.city.format(this.data.city)+ "</td></tr>";
+			//append header to wrapper
+			header.appendChild(name);
+			wrapper.appendChild(header);
+
+		}
+		
 		if (this.config.location === '') {
 			wrapper.innerHTML = "Please set the air quality index <i>location</i> in the config for module: " + this.name + ".";
 			wrapper.className = "dimmed light small";
@@ -65,12 +81,16 @@ Module.register('MMM-AirQuality', {
 			wrapper.className = "dimmed light small";
 			return wrapper;
 		}
-		wrapper.innerHTML = 
+
+		wrapper.innerHTML += 
 			this.html.quality.format(
 				this.html.icon,
 				this.data.impact,
-				(this.config.showIndex?' ('+this.data.value+')':''))+
-			(this.config.showLocation?this.html.city.format(this.data.city):'');
+				(this.config.showIndex?' ('+this.data.value+')':''));
+			if (!this.config.headerNeeded)
+			{
+				wrapper.innerHTML += (this.config.showLocation?this.html.city.format(this.data.city):'');
+			}
 		return wrapper;
 	}
 });
