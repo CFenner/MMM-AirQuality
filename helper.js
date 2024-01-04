79b8ffe203d575ad0ca0488bb64e0a2c55f1a29b
Christopher Fenner <Christopher.Fenner@me.com>
2024-01-04 12:01:39 +0100

    improve logging

--- helper.js
+++ helper.js
@@ -15,22 +15,21 @@ module.exports = {
   },
   loadData: async function (payload) {
     const self = this
-    self.config = payload.config
-    const identifier = payload.identifier
-    const url = `https://${self.config.apiBase}${self.config.dataEndpoint}${self.config.location}/?token=${this.config.token}`
-    console.log(`AirQuality loaded: ${url}`)
+    const url = `https://${payload.config.apiBase}${payload.config.dataEndpoint}${payload.config.location}/?token=${payload.config.token}`
+    console.log(`AirQuality-Fetcher: ${url}`)
 
     const result = await fetch(url)
       .then(response => response.json())
 
     self.sendSocketNotification(self.notifications.DATA_RESPONSE, {
-      payloadReturn: { identifier, result },
+      payloadReturn: { identifier: payload.identifier, result },
       status: 'OK',
     })
   },
   socketNotificationReceived: function (notification, payload) {
     switch (notification) {
       case this.notifications.DATA:
+        console.log(`AirQuality-Fetcher: Loading data of ${payload.config.location} for module ${payload.identifier}`)
         this.loadData(payload)
         break
     }
